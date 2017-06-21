637ab98d0d3d5c97b9c697a69bf9a96d608fad7a
agentd00nut <abrothers656@gmail.com>
2017-06-21 17:11:14 -0400

    Prompt user for username and password, only store temporarily on disk

---
+++ app/js/login.js
@@ -0,0 +1,18 @@
+var fs=require('fs');
+var loc = window.location.pathname;
+var local_dir = loc.substring(0, loc.lastIndexOf('/'));
+
+function set_cookie(){
+
+    username=document.querySelector('[id="username"]');
+    password=document.querySelector('[id="password"]');
+
+    console.log("trying to set coooookie");
+
+    fs.writeFile(local_dir+"/../data/credentials.txt", username.value+";"+password.value,(err)=>{
+        if(err)throw err;     
+        window.location="index.html"
+    });
+
+
+}
